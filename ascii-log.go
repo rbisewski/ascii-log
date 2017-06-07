3f99c5f33c96378bf7deed8f4f380afc1a5ca19c
Robert Bisewski <contact@ibiscybernetics.com>
2017-06-07 15:27:29 -0500

    initial code drop for daemon mode

--- ascii-log.go
+++ ascii-log.go
@@ -50,6 +50,9 @@ var (
 
     // Parameter for the server type
     serverType = ""
+
+    // Argument for enabling daemon mode
+    daemonMode = false
 )
 
 // Initialize the argument input flags.
@@ -58,6 +61,10 @@ func init() {
     // Server type flag
     flag.StringVar(&serverType, "server-type", "nginx",
       "Currently active server; e.g. 'nginx' ")
+
+    // Daemon mode flag
+    flag.BoolVar(&daemonMode, "daemon-mode", false,
+      "Whether or not to run this program as a background service.")
 }
 
 //
@@ -100,126 +107,144 @@ func main() {
     // Assemble the access.log file location.
     access_log_location := log_directory + serverType + "/" + access_log
 
-    // Check if access log file actually exists.
-    byte_contents_of_access_log, err := ioutil.ReadFile(access_log_location)
+    // main infinite loop...
+    for {
 
-    // ensure no error occurred
-    if err != nil {
-        fmt.Println("An error occurred while trying to read the " +
-          "following file: ", access_log_location)
-        fmt.Println(err)
-        os.Exit(1)
-    }
+        // Check if access log file actually exists.
+        byte_contents_of_access_log, err := ioutil.ReadFile(access_log_location)
 
-    // dump the contents of the access log to a string
-    string_contents_of_access_log := string(byte_contents_of_access_log)
-
-    // if the contents are less than 1 byte, end the program
-    if len(string_contents_of_access_log) < 1 {
-        fmt.Println("Note: the following file was empty: ",
-          access_log_location)
-        fmt.Println("Exiting...")
-        os.Exit(0)
-    }
-
-    // Attempt to break up the file into an array of strings a demarked by
-    // the newline character.
-    lines := strings.Split(string_contents_of_access_log, "\n")
-
-    // terminate the program if the array has less than 1 element
-    if len(lines) < 1 {
-        fmt.Println("Warning: no line data was found, exiting...")
-        os.Exit(0)
-    }
-
-    // for every line...
-    for _, line := range lines {
-
-        // attempt to split that line via spaces
-        elements := strings.Split(line, " ")
-
-        // safety check, ensure that element actually has a length
-        // of at least 1
-        if len(elements) < 1 {
-            continue
-        }
-
-        // grab the first element, that is the IP address
-        ip := elements[0]
-
-        // ensure that the ip address is valid length
-        //
-        // 0.0.0.0 --> 8 chars (min)
-        //
-        // 123.123.123.123 --> 15 chars (max)
-        //
-        if len(ip) < 8 || len(ip) > 15 {
-            continue
-        }
-
-        // Since the IP address is indeed roughly valid, go ahead and add
-        // it to the global array.
-        ip_addresses[ip]++
-    }
-
-    // convert the ip addresses map into an array of strings
-    ip_strings, err := convertIpAddressMapToString(ip_addresses)
-
-    // if an error occurred, terminate from the program
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
-
-    // attempt to stat() if the ip.log file even exists
-    _, err = os.Stat(web_location + ip_log)
-
-    // attempt check if the ip.log file exists in the "web_location"
-    if os.IsNotExist(err) {
-
-        // if not, then create it
-        f, creation_err := os.Create(web_location + ip_log)
-
-        // if an error occurred during creation, terminate program
-        if creation_err != nil {
-            fmt.Println(creation_err)
+        // ensure no error occurred
+        if err != nil {
+            fmt.Println("An error occurred while trying to read the " +
+              "following file: ", access_log_location)
+            fmt.Println(err)
             os.Exit(1)
         }
 
-        // then go ahead and close the file connection for the time being
-        f.Close()
-    }
+        // dump the contents of the access log to a string
+        string_contents_of_access_log := string(byte_contents_of_access_log)
 
-    // if an error occurred during stat() then exit the program
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+        // if the contents are less than 1 byte, end the program
+        if len(string_contents_of_access_log) < 1 {
+            fmt.Println("Note: the following file was empty: ",
+              access_log_location)
+            fmt.Println("Exiting...")
+            os.Exit(0)
+        }
 
-    // append the title to the ip_log_contents
-    ip_log_contents += "IP Address Counts Data\n\n"
+        // Attempt to break up the file into an array of strings a demarked by
+        // the newline character.
+        lines := strings.Split(string_contents_of_access_log, "\n")
 
-    // attempt to grab the current day/month/year
-    datetime := time.Now().Format("2017-Jan-01 14:02")
+        // terminate the program if the array has less than 1 element
+        if len(lines) < 1 {
+            fmt.Println("Warning: no line data was found, exiting...")
+            os.Exit(0)
+        }
 
-    // append the date to the ip_log_contents on the next line
-    ip_log_contents += "Log Generated on: " + datetime + "\n"
-    ip_log_contents += "-----------------------------------\n\n"
+        // for every line...
+        for _, line := range lines {
 
-    // append the ip_strings content to this point of the log; it will
-    // either contain the "IPv4 Address + Daily Count" or a message stating
-    // that no addresses appear to be recorded today.
-    ip_log_contents += ip_strings
+            // attempt to split that line via spaces
+            elements := strings.Split(line, " ")
 
-    // attempt to write the string contents to the ip.log file
-    err = ioutil.WriteFile(web_location + ip_log,
-                           []byte(ip_log_contents),
-                           0755)
+            // safety check, ensure that element actually has a length
+            // of at least 1
+            if len(elements) < 1 {
+                continue
+            }
 
-    // if an error occurred, terminate the program
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
+            // grab the first element, that is the IP address
+            ip := elements[0]
+
+            // ensure that the ip address is valid length
+            //
+            // 0.0.0.0 --> 8 chars (min)
+            //
+            // 123.123.123.123 --> 15 chars (max)
+            //
+            if len(ip) < 8 || len(ip) > 15 {
+                continue
+            }
+
+            // Since the IP address is indeed roughly valid, go ahead and add
+            // it to the global array.
+            ip_addresses[ip]++
+        }
+
+        // convert the ip addresses map into an array of strings
+        ip_strings, err := convertIpAddressMapToString(ip_addresses)
+
+        // if an error occurred, terminate from the program
+        if err != nil {
+            fmt.Println(err)
+            os.Exit(1)
+        }
+
+        // attempt to stat() if the ip.log file even exists
+        _, err = os.Stat(web_location + ip_log)
+
+        // attempt check if the ip.log file exists in the "web_location"
+        if os.IsNotExist(err) {
+
+            // if not, then create it
+            f, creation_err := os.Create(web_location + ip_log)
+
+            // if an error occurred during creation, terminate program
+            if creation_err != nil {
+                fmt.Println(creation_err)
+                os.Exit(1)
+            }
+
+            // then go ahead and close the file connection for the time being
+            f.Close()
+        }
+
+        // if an error occurred during stat() then exit the program
+        if err != nil {
+            fmt.Println(err)
+            os.Exit(1)
+        }
+
+        // append the title to the ip_log_contents
+        ip_log_contents += "IP Address Counts Data\n\n"
+
+        // attempt to grab the current day/month/year
+        datetime := time.Now().Format("2017-Jan-01 14:02")
+
+        // append the date to the ip_log_contents on the next line
+        ip_log_contents += "Log Generated on: " + datetime + "\n"
+        ip_log_contents += "-----------------------------------\n\n"
+
+        // append the ip_strings content to this point of the log; it will
+        // either contain the "IPv4 Address + Daily Count" or a message stating
+        // that no addresses appear to be recorded today.
+        ip_log_contents += ip_strings
+
+        // attempt to write the string contents to the ip.log file
+        err = ioutil.WriteFile(web_location + ip_log,
+                               []byte(ip_log_contents),
+                               0755)
+
+        // if an error occurred, terminate the program
+        if err != nil {
+            fmt.Println(err)
+            os.Exit(1)
+        }
+
+        // if daemon mode is disabled, then exit this loop
+        if !daemonMode {
+            break
+        }
+
+        // since the user has selected daemon mode, wait 24 hours
+        // TODO: make this work
+/*
+        for {
+            // grab the time
+        }
+*/
+        break
     }
 
     // If all is well, we can return quietly here.
